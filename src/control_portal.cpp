b56941a872d35a50db75a5bd5005733a94f206b1
Patrick Mania <pmania@tzi.de>
2014-07-18 21:16:03 +0200

    removed comments

--- src/control_portal.cpp
+++ src/control_portal.cpp
@@ -14,16 +14,6 @@ namespace gazebo
     //////////////////////////////////////////////////
     void ControlPortal::OnUpdate(const common::UpdateInfo & /*_info*/)
     {
-       // // Get the box and sphere model
-       // physics::ModelPtr sphere_model = _world->GetModel("sphere1");
-       // physics::ModelPtr box_model = _world->GetModel("box1");
-       // 
-       // // Apply a small linear velocity to the models
-       // sphere_model->SetLinearVel(math::Vector3(.03, 0, 0));
-       // box_model->SetLinearVel(math::Vector3(.03, 0, 0));
-
-      // TODO: Use GetModels and match portal_robot* as valid model name
-      // We can then delete and re-add models at runtime and the code will still work
       physics::ModelPtr portal_robot_model = _world->GetModel("portal_robot");
       if( portal_robot_model != NULL )
       {
@@ -45,17 +35,6 @@ namespace gazebo
       // Get the first world in the simulation
       _world = physics::get_world (); 
       physics::ModelPtr portal_robot_model = _world->GetModel("portal_robot");
-      // if( portal_robot_model != NULL )
-      // {
-      //   physics::JointPtr portal_mover_endeffector_mount_joint = portal_robot_model->GetJoint("portal_mover_endeffector_mount");
-      //   std::cout << "Joint Angle:" << portal_mover_endeffector_mount_joint->GetAngle(0)->Degree() << std::endl;
-      // }
-     // std::cout << "The name of the current world: ";
-     // std::cout << _world->GetName() << std::endl;
-     // physics::PhysicsEnginePtr physics_engine = _world->GetPhysicsEngine ();
-
-     // std::cout << "The physics engine type: ";
-     // std::cout << physics_engine->GetType() << std::endl;
 
      // Subscribe to the World Update event
      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
